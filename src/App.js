bdb79aa98690d8aff0f2c7b19632d65f3f6dffe2
K1nGsmaN-hub <1516342h@gmail.com>
2020-09-11 14:23:48 +0800

    edit files app.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import './App.css';
+import './index.css'
 
 function App() {
   return (
     <div className="App">
+      <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+        <div className="flex-shrink-0">
+        </div>
+        <div className="ml-6 pt-1">
+          <h4 className="text-xl text-gray-900 leading-tight">ChitChat</h4>
+          <p className="text-base text-gray-600 leading-normal">You have a new message!</p>
+        </div>
+      </div>
     </div>
   );
 }
