d3a54990597a6d9930101b9f50bd19b97edcc89a
K1nGsmaN-hub <1516342h@gmail.com>
2020-09-14 21:20:19 +0800

    refactoring visual-composition.js

--- src/components/pages/contacts-page/visual-composition/visual-composition.js
+++ src/components/pages/contacts-page/visual-composition/visual-composition.js
@@ -1,34 +1,60 @@
-import React from "react";
+import React, {Component} from "react";
 
-const VisualComposition = ({contacts, selected}) => {
+import CreateContact from "./create-contact/create-contact";
+
+class VisualComposition extends Component {
+  state = {
+    createMode: false
+  }
+
+  changeMode = () => this.setState(({createMode}) => ({createMode: !createMode}))
+
+  render() {
+    const {selected, contacts, createNewContact} = this.props
+
+    const createModeProps = {
+      createNewContact,
+      changeMode: this.changeMode
+    }
+
+    const contactsView = (this.state.createMode) ? <CreateContact {...createModeProps}/> : contacts
 
     // dinamic selected field view
-  const selectedView = (selected.length > 0) ? selected : <ContactNotSelected />
-  const selectedFieldClassList = (selected.length === 0) ? 'h-full flex justify-center items-center' : ''
+    const selectedView = (selected.length > 0) ? selected : <ContactNotSelected />
+    const selectedFieldClassList = (selected.length === 0) ? 'h-full flex justify-center items-center' : ''
 
+    const btnClassList = 'box-border px-3 bg-transparent hover:bg-gray-400 active:border-none ' +
+      'rounded text-xl text-gray-700 text-center'
 
-  return (
-    <div className="contacts-page mt-32 px-4 py-4 flex justify-between shadow-md bg-gray-200">
+    return (
+      <div className="contacts-page mt-32 px-4 py-4 flex justify-between shadow-md bg-gray-200">
 
-      <aside className="w-1/4 pr-4" style={{borderRight: '1px solid #a0aec0', minHeight: '600px'}}>
-        <h3 className="text-2xl">Контакты</h3>
-        <div className={`contacts-container divide-y divide-gray-400`}>
-          {contacts}
-        </div>
-      </aside>
+        <aside className="w-1/3 pr-4" style={{borderRight: '1px solid #a0aec0', height: '600px'}}>
+          <div className="flex justify-between">
+            <h3 className="text-2xl">Контакты</h3>
+            <button onClick={this.changeMode} className={btnClassList}>
+              <span>+</span>
+            </button>
+          </div>
+          <div className={`contacts-container divide-y divide-gray-400 overflow-y-auto`} style={{height: '560px'}}>
+            {contactsView}
+          </div>
+        </aside>
 
-      <article className="w-3/4 pl-4">
-        <h3 className="text-2xl">Подробнее</h3>
-        <div className={selectedFieldClassList}>
-          {selectedView}
-        </div>
-      </article>
-    </div>
-  )
+        <article className="w-2/3 pl-4">
+          <h3 className="text-2xl">Подробнее</h3>
+          <div className={selectedFieldClassList}>
+            {selectedView}
+          </div>
+        </article>
+      </div>
+    )
+  }
+
 }
 
 const ContactNotSelected = () => {
-  return <span>Пожалуйста, выберете контакт из списка.</span>
+  return <span>Пожалуйста, выберете контакт.</span>
 }
 
 
