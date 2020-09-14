d26be6dba540f7d1751f8e6682db14af8f7f95cc
K1nGsmaN-hub <1516342h@gmail.com>
2020-09-14 13:21:31 +0800

    refactoring visual-composition.js

--- src/components/pages/contacts-page/visual-composition/visual-composition.js
+++ src/components/pages/contacts-page/visual-composition/visual-composition.js
@@ -2,34 +2,23 @@ import React from "react";
 
 const VisualComposition = ({contacts, selected}) => {
 
-  // dinamic contacts view
-  const contactsView = (contacts.length > 0) ? contacts : <EmptyContacts/>
-  const contactsContainerClassList =
-    (contacts.length === 0) ? 'h-full flex justify-center items-center divide-y divide-gray-400' : ''
-
-  // dinamic selected field view
+    // dinamic selected field view
   const selectedView = (selected.length > 0) ? selected : <ContactNotSelected />
-  const selectedFieldClassList =
-    (selected.length === 0) ? 'h-full flex justify-center items-center' : ''
+  const selectedFieldClassList = (selected.length === 0) ? 'h-full flex justify-center items-center' : ''
+
 
   return (
     <div className="contacts-page mt-32 px-4 py-4 flex justify-between shadow-md bg-gray-200">
-      {/* left side */}
-      <aside className="w-1/4 pr-4" style={{
-        borderRight: '1px solid #a0aec0',
-        minHeight: '600px'
-      }}>
-        <h3 className="text-2xl">Контакты</h3>
 
-        <div className={`contacts-container ${contactsContainerClassList}`}>
-          {contactsView}
+      <aside className="w-1/4 pr-4" style={{borderRight: '1px solid #a0aec0', minHeight: '600px'}}>
+        <h3 className="text-2xl">Контакты</h3>
+        <div className={`contacts-container divide-y divide-gray-400`}>
+          {contacts}
         </div>
       </aside>
 
-      {/* right side */}
       <article className="w-3/4 pl-4">
         <h3 className="text-2xl">Подробнее</h3>
-
         <div className={selectedFieldClassList}>
           {selectedView}
         </div>
@@ -40,16 +29,6 @@ const VisualComposition = ({contacts, selected}) => {
 
 const ContactNotSelected = () => {
   return <span>Пожалуйста, выберете контакт из списка.</span>
-}
-
-const EmptyContacts = () => {
-  return (
-    <button
-      className="box-border px-4 py-3 mx-auto bg-blue-500 hover:bg-blue-600 active:border-none rounded text-white text-center font-bold"
-    >
-      Добавить контакт
-    </button>
-  )
 }
 
 
