563a26ad1213dee4b2231b24f42590d51ed579c9
K1nGsmaN-hub <1516342h@gmail.com>
2020-09-14 21:17:38 +0800

    refactoring contacts-item.js

--- src/components/pages/contacts-page/visual-composition/contacts-item/contacts-item.js
+++ src/components/pages/contacts-page/visual-composition/contacts-item/contacts-item.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-import userAvatar from '../../../../../user-avatar.webp'
+import ViewMode from "./view-mode/view-mode";
 
-const ContactsItem = ({imgAvatar, fullName, id, setContactActive}) => {
-  return (
-    <div onClick={() => setContactActive(id)} className="contacts-item w-full h-20 flex items-center cursor-pointer">
-      <div className="contacts-item__avatar ml-2 mr-6">
-        <img
-          className="w-12 rounded-full"
-          src={(imgAvatar) ? imgAvatar : userAvatar} alt="user-avatar"
-        />
-      </div>
-      <div className="contacts-item__name">
-        <span className="text-lg tracking-wider">{fullName}</span>
-      </div>
-    </div>
-  )
+class ContactsItem extends Component {
+
+  render() {
+    const viewModeProps = {
+      ...this.props
+    }
+
+    return (
+      <>
+        <ViewMode {...viewModeProps} />
+      </>
+    )
+  }
 }
+
 
 export default ContactsItem
