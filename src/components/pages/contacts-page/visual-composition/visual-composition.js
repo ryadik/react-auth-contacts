be9d64ded81452f48194f6b863c2829496083758
K1nGsmaN-hub <1516342h@gmail.com>
2020-09-15 18:00:29 +0800

    Added search field and logout button in visual-composition.js

--- src/components/pages/contacts-page/visual-composition/visual-composition.js
+++ src/components/pages/contacts-page/visual-composition/visual-composition.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {withRouter} from 'react-router-dom'
 
 import CreateContact from "./create-contact/create-contact";
 
@@ -10,7 +11,7 @@ class VisualComposition extends Component {
   changeMode = () => this.setState(({createMode}) => ({createMode: !createMode}))
 
   render() {
-    const {selected, contacts, createNewContact} = this.props
+    const {selected, contacts, createNewContact, setSearchInputValue, onLogout} = this.props
 
     const createModeProps = {
       createNewContact,
@@ -23,26 +24,44 @@ class VisualComposition extends Component {
     const selectedView = (selected.length > 0) ? selected : <ContactNotSelected />
     const selectedFieldClassList = (selected.length === 0) ? 'h-full flex justify-center items-center' : ''
 
-    const btnClassList = 'box-border px-3 bg-transparent hover:bg-gray-400 active:border-none ' +
-      'rounded text-xl text-gray-700 text-center'
+    const btnClassList = 'box-border px-3 bg-transparent active:border-none rounded text-center'
 
     return (
-      <div className="contacts-page mt-32 px-4 py-4 flex justify-between shadow-md bg-gray-200">
+      <div className="contacts-page mt-32 px-4 py-4 flex justify-between shadow-md bg-gray-200 ">
 
-        <aside className="w-1/3 pr-4" style={{borderRight: '1px solid #a0aec0', height: '600px'}}>
+        <aside className="w-1/3 pr-4 divide-y divide-gray-400" style={{borderRight: '1px solid #a0aec0', height: '600px'}}>
           <div className="flex justify-between">
             <h3 className="text-2xl">Контакты</h3>
-            <button onClick={this.changeMode} className={btnClassList}>
+            <button onClick={this.changeMode}
+                    title="Добавить"
+                    className={btnClassList + ' hover:bg-gray-400 text-xl text-gray-700'}>
               <span>+</span>
             </button>
           </div>
-          <div className={`contacts-container divide-y divide-gray-400 overflow-y-auto`} style={{height: '560px'}}>
+          <div className="search">
+            <input
+              onChange={(e) => setSearchInputValue(e)}
+              className="box-border px-4 py-3 my-3 w-full mx-auto border-2 border-solid border-gray-500 hover:border-gray-600 focus:border-gray-600 rounded"
+              placeholder="Поиск контактов"
+              type="text"/>
+          </div>
+          <div className={`contacts-container divide-y divide-gray-400 overflow-y-auto`} style={{height: '485px'}}>
             {contactsView}
           </div>
         </aside>
 
-        <article className="w-2/3 pl-4">
-          <h3 className="text-2xl">Подробнее</h3>
+        <article className="w-2/3 pl-4 divide-y divide-gray-400">
+          <div className="flex justify-between">
+            <h3 className="text-2xl">Подробнее</h3>
+            <button
+              onClick={() => {
+                onLogout()
+                this.props.history.push('/')
+              }}
+              className={btnClassList + ' h-8 bg-red-600 hover:bg-red-700'}>
+              <span className="leading-3 text-sm text-gray-100 font-normal">Выход</span>
+            </button>
+          </div>
           <div className={selectedFieldClassList}>
             {selectedView}
           </div>
@@ -58,4 +77,4 @@ const ContactNotSelected = () => {
 }
 
 
-export default VisualComposition
+export default withRouter(VisualComposition)
